adef29b26604c5a6a674b5a32887a62e6e8f525d
FedeVinc <100800969+FedeVinc@users.noreply.github.com>
2022-03-03 09:10:53 +0100

    Create setup_rpi_for_tflite.sh

---
+++ setup_rpi_for_tflite.sh
@@ -0,0 +1,43 @@
+# Show your Raspberry Pi OS version.
+cat /etc/os-release
+
+# Update packages on your Raspberry Pi OS.
+sudo apt-get update
+
+# Check your Python version. You should have Python 3.7 or later.
+python3 --version
+
+# Install virtualenv and upgrade pip.
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+
+# Create a Python virtual environment for the TFLite samples (optional but strongly recommended)
+python3 -m venv ~/tflite
+
+# Run this command whenever you open a new Terminal window/tab to activate the environment.
+source ~/tflite/bin/activate
+
+#Install requirements; some of these packages are for the GUI components of the OpenCV
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev python3-pyqt5 libatlas-base-dev libjasper-dev
+
+#Install OpenCV
+pip install opencv-contrib-python==4.5.3.56
+
+#Install Tensorflow Lite for Python
+import tflite_runtime.interpreter as tflite
+
+# Clone the TensorFlow example repository with the TFLite Raspberry Pi samples.
+git clone https://github.com/tensorflow/examples.git
+cd examples/lite/examples/object_detection/raspberry_pi
+
+# Install dependencies required by the sample
+sh setup.sh
+
+# Run the object detection sample
+python detect.py
+
+####
+# If you see an error running the sample:
+# ImportError: libcblas.so.3: cannot open shared object file: No such file or directory
+# you can fix it by installing an OpenCV dependency that is missing on your Raspberry Pi.
+sudo apt-get install libatlas-base-dev
